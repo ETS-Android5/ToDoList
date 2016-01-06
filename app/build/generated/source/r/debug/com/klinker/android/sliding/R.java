71bd1656f84a45e9d494589f4caf2b59a05526f8
DWA2112 <service@die-web-agenten.de>
2016-01-06 22:05:49 +0100

    app in progress, still not working

--- app/build/generated/source/r/debug/com/klinker/android/sliding/R.java
+++ app/build/generated/source/r/debug/com/klinker/android/sliding/R.java
@@ -554,9 +554,9 @@ public final class R {
 		public static final int abc_textfield_search_activated_mtrl_alpha = 0x7f020042;
 		public static final int abc_textfield_search_default_mtrl_alpha = 0x7f020043;
 		public static final int abc_textfield_search_material = 0x7f020044;
-		public static final int design_fab_background = 0x7f020075;
-		public static final int design_snackbar_background = 0x7f020076;
-		public static final int notification_template_icon_bg = 0x7f0200ab;
+		public static final int design_fab_background = 0x7f020074;
+		public static final int design_snackbar_background = 0x7f020075;
+		public static final int notification_template_icon_bg = 0x7f0200a1;
 	}
 	public static final class id {
 		public static final int action0 = 0x7f0c00b4;
